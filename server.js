bb9b8aeb4f1445a527ac62ce9cb338d195fc1483
sam11385 <samluedke@gmail.com>
2021-01-21 21:04:41 -0700

    get server going because I dunno how to do anything

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const cors = require("cors");
+const http = require('http');
+const axios = require('axios');
+
+
 const app = express();
 
 
 // Database connection parameters:
-const config = {
-    host: 'localhost',
-    port: 5432,
-    database: 'restaurant',
-    user: 'postgres'
-};
-  
-  
-app.get('/', function(req, res) {
-    res.render('home', {
-        pageTitle: 'Home',
-        pageID: 'Home'
-    });
+// const config = {
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'restaurant',
+//     user: 'postgres'
+// };
+ 
+app.use(cors());
+ 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
 });
+ 
+app.listen(3000, () =>
+  console.log(`Example app listening on port 3000!`),
+);
